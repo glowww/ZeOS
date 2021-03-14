98708991f9d17bd26b3cbf89a72e4b4b0a93e8b7
Juanse Brito <juansebr98@gmail.com>
2021-03-14 18:38:29 +0100

    Merging

--- user.c
+++ user.c
@@ -15,13 +15,6 @@ int __attribute__ ((__section__(".text.main")))
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-<<<<<<< HEAD
     
   while(1) { }
 }
-=======
-	int myLocalVariable = add(0x42, 0x66);  
-  	return myLocalVariable;//while(1) { }
-}
-
->>>>>>> 76e1fb7 (E1.check (sin wrapper aun))
